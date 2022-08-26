629dac3c7183f89653a4a283a31f1e2de58c9cc4
pjuhas <maybehero@protonmail.ch>
2022-08-26 10:48:54 +0200

    bug fix

--- app/src/main/java/sk/upjs/vma/runningpacer/feature_vdot/presentation/listOfRuns/TrainingPaceScreen.kt
+++ app/src/main/java/sk/upjs/vma/runningpacer/feature_vdot/presentation/listOfRuns/TrainingPaceScreen.kt
@@ -87,7 +87,13 @@ fun TrainingPaceScreen(
                 containerColor = MaterialTheme.colorScheme.primary,
                 contentColor = MaterialTheme.colorScheme.inverseOnSurface,
                 onClick = {
-                    navController.navigate(Screen.AddRun.route)
+                    navController.navigate(Screen.AddRun.route) {
+                        popUpTo(navController.graph.findStartDestination().id) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
                 }
             ) { Icon(imageVector = Icons.Default.Add, contentDescription = "Add") }
         }
